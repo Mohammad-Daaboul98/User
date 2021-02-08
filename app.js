e5eaa3b3c163cda9bab798562d562e064f493c04
Recon <mohammad.daaboul.99@gmail.com>
2021-02-08 11:10:30 +0200

    Add bcrypt

--- app.js
+++ app.js
@@ -2,66 +2,75 @@ require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const mongoose =require("mongoose");
+const mongoose = require("mongoose");
 // const encrypt = require('mongoose-encryption'); 
-const md5 = require("md5")l
+// const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRound = 10;
 
 const app = express();
 
 app.use(express.static("Public"));
-app.set("view engine","ejs");
-app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
-const userSchema = new mongoose.Schema ({
-email:String,
-password:String
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
 });
- 
+
 const secret = process.env.SECRET;
 // userSchema.plugin(encrypt,{secret:secret,encryptedFields:["password"]});
 
-const User =  mongoose.model("User",userSchema);
+const User = mongoose.model("User", userSchema);
 
-app.get("/",function(req,res){
-res.render("home");
+app.get("/", function (req, res) {
+    res.render("home");
 });
 
-app.get("/login",function(req,res){
+app.get("/login", function (req, res) {
     res.render("login");
- });
+});
 
 
-app.get("/register",function(req,res){
-     res.render("register");
- });
-        
- app.post("/register",function(req,res){
-     const newUser = new User({
-         email:req.body.username,
-         password:md5(req.body.password);
-     });
+app.get("/register", function (req, res) {
+    res.render("register");
+});
 
-     newUser.save(function(err){
-         if(err){
-             console.log(err);
-         }else{
-             res.render("secrets");
-         }
-     });
- });
+app.post("/register", function (req, res) {
+    bcrypt.hash(req.body.password, saltRound, function (err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+
+        newUser.save(function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("secrets");
+            }
+        });
+    });
+
+});
 
 
-app.post("/login",function(req,res){
+app.post("/login", function (req, res) {
     const userName = req.body.username;
-    const password =md5(req.body.password);
-    User.findOne({email:userName},function(err,foundUser){
-        if(!err){
-            if(foundUser){
-                if(foundUser.password === password){
-                    res.render("secrets")
-                }
+    const password = req.body.password
+    User.findOne({ email: userName }, function (err, foundUser) {
+        if (!err) {
+            if (foundUser) {
+                bcrypt.compare(password, foundUser.password, function (err, result) {
+                    if (result === true) {
+                        res.render("secrets")
+                    }
+                });
+
+
             }
         }
     });
@@ -72,6 +81,6 @@ app.post("/login",function(req,res){
 
 
 
-app.listen("3000",function(req,res){
+app.listen("3000", function (req, res) {
     console.log("server is running on port 3000");
 });
